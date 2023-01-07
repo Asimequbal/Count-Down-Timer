be7db260a95606c2dea0fee0ce7df1b02c5cb106
Asim Equbal <114874702+Asimequbal@users.noreply.github.com>
2023-01-07 09:21:39 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-let Element = document.querySelector('.container')
-let EndCount = new Date('January 25 2023 10:00:00').getTime()
+let Element = document.querySelector('.inner_container')
+let EndCount = new Date('January 30 2023 03:00:00').getTime()
+// console.log(EndCount)
 setInterval(() =>{
 
-}) 
-document.querySelector
+
+let currentTime = new Date().getTime()    
+let Time = EndCount - currentTime
+// console.log(Time)
+let days = Math.floor(Time/(1000*60*60*24))
+let hours = Math.floor((Time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+let minutes = Math.floor((Time % (1000 * 60 * 60)) / (1000 * 60))
+let seconds = Math.floor((Time % (1000 * 60)) / 1000)
+
+
+console.log(currentTime)
+
+document.querySelector('#Days').innerText = days
+document.querySelector('#Hours').innerText = hours
+document.querySelector('#Minutes').innerText = minutes
+document.querySelector('#Seconds').innerText = seconds
+
+// element.innerHTML = days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds '
+
+if (time < 0) {
+    element.innerHTML = "Time is up!!!"
+}
+
+}, 1000);
+
+
